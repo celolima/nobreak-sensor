0cce5631c47abc2382cdf2026d456835ea65f1b0
Marcelo Lima <celorsl@yahoo.com.br>
2018-02-26 15:03:11 -0300

    .

--- client/public/index.js
+++ client/public/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import DevicesDashboard from './DevicesDashboard';
-import registerServiceWorker from './registerServiceWorker';
 import './static/App.css';
 import './static/semantic-dist/semantic.css';
 import './static/vendor/uuid.js';
 import './static/vendor/babel-standalone.js';
 
-ReactDOM.render(<DevicesDashboard />, document.getElementById('root'));
-registerServiceWorker();
+Alert('TESTe');
+
+ReactDOM.render(<DevicesDashboard />, document.getElementById('content'));
