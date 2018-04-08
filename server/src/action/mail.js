b49a824b8021ed733cbf68880e4ddb3c26b04e04
marcelo <m_lima@bitbucket.org>
2018-04-08 17:44:40 -0300

    adiciona package de mail

---
+++ server/src/action/mail.js
@@ -0,0 +1,23 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+           user: 'tel73n@gmail.com',
+           pass: 'eletronica*123'
+       }
+});
+
+const mailOptions = {
+    from: 'tel73n@gmail.com', // sender address
+    to: 'celorslima@gmail.com', // list of receivers
+    subject: 'Email de teste from NODEMAILER', // Subject line
+    html: '<p>Hello There, test</p>'// plain text body
+};
+
+transporter.sendMail(mailOptions, function (err, info) {
+if(err)
+    console.log(err)
+else
+    console.log(info);
+});
